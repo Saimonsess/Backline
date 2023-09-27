448f11295a864ae926d8c79a57fcc6a5ed3fbb37
Saimonses <Simonky_6@hotmail.com>
2023-09-27 15:14:27 -0300

    a

--- app/src/main/java/com/log/instrumentos/Carga.java
+++ app/src/main/java/com/log/instrumentos/Carga.java
@@ -1,6 +1,17 @@
 package com.log.instrumentos;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
 
-public class Carga extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Carga extends AppCompatActivity {
+    private ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga);
+        progressBar = findViewById(R.id.progressBar);
+    }
 }
