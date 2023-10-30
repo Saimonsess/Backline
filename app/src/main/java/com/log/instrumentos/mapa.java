9376d23c4d270eacf09cf8c4f11d737a3d4e2fff
Saimonses <Simonky_6@hotmail.com>
2023-10-30 02:52:14 -0300

    v1.0

---
+++ app/src/main/java/com/log/instrumentos/mapa.java
@@ -0,0 +1,49 @@
+
+package com.log.instrumentos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class mapa extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+    EditText txtLatitud, txtLongitud;
+    GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa);
+        txtLatitud = findViewById(R.id.txtLatitud);
+        txtLongitud = findViewById(R.id.txtLongitud);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap=googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng santiago = new LatLng(-33.4515981,-70.6619977);
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(""+latLng.latitude);
+        txtLongitud.setText(""+latLng.longitude);
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(""+latLng.latitude);
+        txtLongitud.setText(""+latLng.longitude);
+    }
+}
--- app/src/main/java/com/log/instrumentos/seleccion.java
+++ app/src/main/java/com/log/instrumentos/seleccion.java
@@ -8,29 +8,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class seleccion extends AppCompatActivity {
     Button btn;
     Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.login);
-        btn2 = findViewById(R.id.regis);
+        setContentView(R.layout.activity_seleccion);
+        btn = findViewById(R.id.mapa);
+        btn2 = findViewById(R.id.login2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,seleccion.class);
+                Intent intent = new Intent(seleccion.this,mapa.class);
                 startActivity(intent);
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Registro.class);
+                Intent intent = new Intent(seleccion.this,Instrumentos.class);
                 startActivity(intent);
             }
         });
     }
-
 }
